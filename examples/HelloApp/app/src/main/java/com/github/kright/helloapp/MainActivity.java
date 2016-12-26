f07980e7b298f6af8951161bba56652bb311f35c
Igor Slobodskov <igor.slobodskov@phystech.edu>
2016-12-26 15:42:31 +0300

    fix for travis

--- examples/HelloApp/app/src/main/java/com/github/kright/helloapp/MainActivity.java
+++ examples/HelloApp/app/src/main/java/com/github/kright/helloapp/MainActivity.java
@@ -1,9 +1,9 @@
 package com.github.kright.helloapp;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
