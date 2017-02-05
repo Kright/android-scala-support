81373b6b13ef067fbff22a3beff518009f73874f
Igor Slobodskov <igor.slobodskov@phystech.edu>
2017-02-05 13:10:37 +0300

    import assertEquals

--- examples/HelloScalaApp/app/src/androidTest/java/com/github/kright/helloscalaapp/ApplicationScalaTest.scala
+++ examples/HelloScalaApp/app/src/androidTest/java/com/github/kright/helloscalaapp/ApplicationScalaTest.scala
@@ -2,12 +2,11 @@ package com.github.kright.helloscalaapp
 
 import android.test.ActivityInstrumentationTestCase2
 import android.widget.TextView
+import junit.framework.Assert._
 
 class ApplicationScalaTest extends ActivityInstrumentationTestCase2[MainActivity](classOf[MainActivity]) {
 	def testActivityContent(): Unit = {
 		val textView = getActivity.findViewById(R.id.text_view_hello).asInstanceOf[TextView]
-		// assertEquals(textView.getText, "Hello World!")
-		// todo: fix it
-		assert(textView.getText == "Hello World!")
+		assertEquals(textView.getText, "Hello World!")
 	}
 }
