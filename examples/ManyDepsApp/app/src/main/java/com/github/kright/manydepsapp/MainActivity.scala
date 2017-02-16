c57bfa52505296dc125fd495df27d73a57223a31
Igor Slobodskov <igor.slobodskov@phystech.edu>
2017-02-16 23:24:30 +0300

    Add location from google api

--- examples/ManyDepsApp/app/src/main/java/com/github/kright/manydepsapp/MainActivity.scala
+++ examples/ManyDepsApp/app/src/main/java/com/github/kright/manydepsapp/MainActivity.scala
@@ -2,15 +2,61 @@ package com.github.kright.manydepsapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.common.api.GoogleApiClient.{ConnectionCallbacks, OnConnectionFailedListener}
+import com.google.android.gms.location.LocationServices
 
 class MainActivity extends AppCompatActivity {
+
+	var googleApiClient: GoogleApiClient = null
+
 	override protected def onCreate(savedInstanceState: Bundle) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		if (googleApiClient == null) {
+			googleApiClient = new GoogleApiClient.Builder(this).
+				addConnectionCallbacks(new CustomConnectionsCallbacks()).
+				addOnConnectionFailedListener(new CustomOnConnectionFailedListener).
+				addApi(LocationServices.API).
+				build()
+		}
 	}
 
-	override protected def onResume {
-		super.onResume
-		AppActors.helloActor ! "resume"
+	override def onStart(): Unit = {
+		googleApiClient.connect()
+		super.onStart()
+	}
+
+	override def onStop(): Unit = {
+		googleApiClient.disconnect()
+		super.onStop()
+	}
+
+	override def onResume(): Unit = {
+		super.onResume()
+		AppActors.logActor ! "resume"
+	}
+
+
+	class CustomConnectionsCallbacks extends ConnectionCallbacks {
+		override def onConnectionSuspended(i: Int): Unit = AppActors.logActor ! "onConnectionSuspended"
+
+		override def onConnected(bundle: Bundle): Unit = {
+			AppActors.logActor ! "onConnected"
+
+			val lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient)
+			if (lastLocation != null)
+				AppActors.logActor ! s"location(${lastLocation.getLatitude}, ${lastLocation.getLongitude})"
+			else
+				AppActors.logActor ! s"location is null"
+		}
+	}
+
+	class CustomOnConnectionFailedListener extends OnConnectionFailedListener {
+		override def onConnectionFailed(connectionResult: ConnectionResult): Unit = {
+			AppActors.logActor ! connectionResult
+		}
 	}
 }
