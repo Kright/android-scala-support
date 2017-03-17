6dfa33457e3e41875227d279e3bfe45379e17a5b
Igor Slobodskov <igor.slobodskov@phystech.edu>
2017-03-17 23:02:08 +0300

    add new project with proguard

---
+++ examples/ProguardApp/app/src/main/java/com/github/kright/proguardapp/MainActivity.scala
@@ -0,0 +1,12 @@
+package com.github.kright.proguardapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity extends AppCompatActivity {
+
+	override def onCreate(savedInstanceState: Bundle): Unit = {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
