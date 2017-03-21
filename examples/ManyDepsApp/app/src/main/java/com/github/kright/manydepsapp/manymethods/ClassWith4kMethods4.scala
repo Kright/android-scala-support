e2d23164eb88ea2733aa5076eaa4d2b12cb00f90
Igor Slobodskov <igor.slobodskov@phystech.edu>
2017-03-21 22:58:32 +0300

    add more unused classes (64k methods in sum)

--- examples/ManyDepsApp/app/src/main/java/com/github/kright/manydepsapp/manymethods/ClassWith4kMethods4.scala
+++ examples/ManyDepsApp/app/src/main/java/com/github/kright/manydepsapp/manymethods/ClassWith4kMethods4.scala
@@ -7,5 +7,53 @@ package com.github.kright.manydepsapp.manymethods
 	*/
 class ClassWith4kMethods4 {
 
-	// def log[@specialized A, @specialized B, @specialized C, @specialized D](a: A, b: B, c: C, d: D): Unit = {}
+	def log[@specialized A, @specialized B, @specialized C, @specialized D](a: A, b: B, c: C, d: D): Unit = {}
+}
+
+class ClassWith4kMethods5 {
+	def log[@specialized A, @specialized B, @specialized C, @specialized D](a: A, b: B, c: C, d: D): Unit = {}
+}
+
+class ClassWith4kMethods6 {
+	def log[@specialized A, @specialized B, @specialized C, @specialized D](a: A, b: B, c: C, d: D): Unit = {}
+}
+
+class ClassWith4kMethods7 {
+	def log[@specialized A, @specialized B, @specialized C, @specialized D](a: A, b: B, c: C, d: D): Unit = {}
+}
+
+class ClassWith4kMethods8 {
+	def log[@specialized A, @specialized B, @specialized C, @specialized D](a: A, b: B, c: C, d: D): Unit = {}
+}
+
+class ClassWith4kMethods9 {
+	def log[@specialized A, @specialized B, @specialized C, @specialized D](a: A, b: B, c: C, d: D): Unit = {}
+}
+
+class ClassWith4kMethods10 {
+	def log[@specialized A, @specialized B, @specialized C, @specialized D](a: A, b: B, c: C, d: D): Unit = {}
+}
+
+class ClassWith4kMethods11 {
+	def log[@specialized A, @specialized B, @specialized C, @specialized D](a: A, b: B, c: C, d: D): Unit = {}
+}
+
+class ClassWith4kMethods12 {
+	def log[@specialized A, @specialized B, @specialized C, @specialized D](a: A, b: B, c: C, d: D): Unit = {}
+}
+
+class ClassWith4kMethods13 {
+	def log[@specialized A, @specialized B, @specialized C, @specialized D](a: A, b: B, c: C, d: D): Unit = {}
+}
+
+class ClassWith4kMethods14 {
+	def log[@specialized A, @specialized B, @specialized C, @specialized D](a: A, b: B, c: C, d: D): Unit = {}
+}
+
+class ClassWith4kMethods15 {
+	def log[@specialized A, @specialized B, @specialized C, @specialized D](a: A, b: B, c: C, d: D): Unit = {}
+}
+
+class ClassWith4kMethods16 {
+	def log[@specialized A, @specialized B, @specialized C, @specialized D](a: A, b: B, c: C, d: D): Unit = {}
 }
