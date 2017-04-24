8f8c7f45ad54dbc4f878f9b4001db2cce4297410
Igor Slobodskov <igor.slobodskov@phystech.edu>
2017-04-24 21:24:38 +0300

    add simple example app

---
+++ examples/Scala212RertolambdaSimpleApp/app/src/main/java/com/github/kright/helloapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.github.kright.helloapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
